2e5951c258d49a1e34c96b0e8af0da076307a0dc
AkhilPoojary <akhilsru3@gmail.com>
2024-05-17 23:08:28 +0530

    part time employee

--- src/main/java/org/example/EmployeeServiceImple.java
+++ src/main/java/org/example/EmployeeServiceImple.java
@@ -1,26 +1,36 @@
 package org.example;
 
 import java.util.Random;
+import java.util.Scanner;
 
 import static java.lang.Math.random;
 
-public class EmployeeServiceImple implements EmployeeService{
+public class EmployeeServiceImple implements EmployeeService {
     @Override
     public boolean isEmployeeAvailabale() {
-        Random random=new Random();
+        Random random = new Random();
         return random.nextBoolean();
     }
 
     @Override
     public int checkWagePerDay() {
-        if(isEmployeeAvailabale()) {
+        if (isEmployeeAvailabale()) {
             int wagePerHour = 20;
-            System.out.println("daily employee wage is " + 20 * 8);
-            return 20 * 8;
-        }
-        else {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("employee is part time or full time employee");
+            int choice = sc.nextInt();
+            if (choice == 1) {
+                System.out.println("employee is full time worker");
+                System.out.println("daily employee wage is " + 20 * 8);
+                return 20 * 8;
+            } else if (choice == 2) {
+                System.out.println("employee is part time worker");
+                System.out.println("daily employee wage is " + 20 * 4);
+                return 20 * 4;
+            }
+        } else {
             System.out.println("employee is absent ");
-            return 0;
         }
+        return 0;
     }
 }
